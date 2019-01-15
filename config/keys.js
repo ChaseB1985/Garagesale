0e748112ab69b91d9e9561f499eb9068060e8a66
chaseb1985 <chasebarrier85@gmail.com>
2019-01-15 08:46:40 -0600

    hooking up mongo/goose

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
     googleClientID: '836666616849-9qc77ijsrqagucthc0mfjk9tcbnid9in.apps.googleusercontent.com',
-    googleClientSecret: 'iV5Is4hpx6UMIfMs0w60twwk'
+    googleClientSecret: 'iV5Is4hpx6UMIfMs0w60twwk',
+    mongoURI: 'mongodb://localhost/mongopractice'
 }
--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 const express = require('express');
+const mongoose = require('mongoose');
 require('./services/passport');
 const app = express(); 
+const keys = require('./config/keys')
+
+mongoose.connect(keys.mongoURI);
 
 require('./routes/authRoutes')(app);
 
