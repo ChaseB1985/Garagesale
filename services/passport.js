a50d46f93703a37bee514593d20bb1f93e3ed8e9
chaseb1985 <chasebarrier85@gmail.com>
2019-01-16 14:31:23 -0600

    oauth was working

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,6 @@
 const passport = require('passport');
-const mongoose = require('mongoose');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
 
 const User = mongoose.model('users');
@@ -16,20 +16,22 @@ passport.deserializeUser((id, done) => {
     });
 });
 
-passport.use(new GoogleStrategy({
+passport.use(
+    new GoogleStrategy(
+    {
     clientID: keys.googleClientID, 
     clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
+    callbackURL: '/auth/google/callback',
+    proxy: true
     }, 
     async (accessToken, refreshToken, profile, done) => {
-        const existingUser = await User.findOne({ googleId: profile.id })
-            if (exisingUser){
+        const existingUser = await User.findOne({ googleId: profile.id });
+
+            if (existingUser){
                 return done(null, existingUser);
             } 
-                const user = await new User({ googleId: profile.id }).save()
-                done(null, user);                
-            
-         
+                const user = await new User({ googleId: profile.id }).save();
+                done(null, user);               
         }
     )  
 );
