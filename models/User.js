77d411ad2971d03f849b6cc9f29b91b2ca42d96e
chaseb1985 <chasebarrier85@gmail.com>
2019-02-07 15:28:41 -0600

    working on google

--- models/User.js
+++ models/User.js
@@ -4,60 +4,9 @@ const { Schema } = mongoose;
 
 const userSchema = new Schema({
 	username: String,
-    googleId: String, 
+    googleID: String, 
     credits: { type: Number, default: 0 }
 });
 
-const User = mongoose.model('users', userSchema);
-module.exports = User;
-//module.exports = mongoose.model('users', userSchema);
-
-// const userSchema = new Schema({
-// 	firstName: { type: String, unique: false },
-// 	lastName: { type: String, unique: false },
-// 	local: {
-// 		username: { type: String, unique: false, required: false },
-// 		password: { type: String, unique: false, required: false }
-// 	},
-// 	google: {
-// 		googleId: { type: String, required: false }
-// 	},
-	//photos: []
-	// local: {
-	// 	email: { type: String, unique: true },
-	// 	password: { type: String }
-	// },
-	// google: {
-	// 	id: { type: String },
-	// 	photos: []
-	// },
-	// firstName: { type: String },
-	// lastName: { type: String }
-//})
-
-// Define schema methods
-// userSchema.methods = {
-// 	checkPassword: function(inputPassword) {
-// 		return bcrypt.compareSync(inputPassword, this.local.password)
-// 	},
-// 	hashPassword: plainTextPassword => {
-// 		return bcrypt.hashSync(plainTextPassword, 10)
-// 	}
-// }
-
-// Define hooks for pre-saving
-// userSchema.pre('save', function(next) {
-// 	if (!this.local.password) {
-// 		console.log('=======NO PASSWORD PROVIDED=======')
-// 		next()
-// 	} else {
-// 		this.local.password = this.hashPassword(this.local.password)
-// 		next()
-// 	}
-	// this.password = this.hashPassword(this.password)
-	// next()
-//})
-
-// Create reference to User & export
-// const User = mongoose.model('users', userSchema)
-// module.exports = User
+//const User = mongoose.model('users', userSchema);
+mongoose.model('users', userSchema);
