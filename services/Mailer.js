03b7c3ec876829883f636fd55475f71eddd812bb
chaseb1985 <chasebarrier85@gmail.com>
2019-02-12 11:50:09 -0600

    almost there

--- services/Mailer.js
+++ services/Mailer.js
@@ -1,6 +1,6 @@
 const sendgrid = require('sendgrid');
 const helper = sendgrid.mail;
-const keys = require('../config/keys');
+const keys = require('../keychain/keys');
 
 class Mailer extends helper.Mail {
     constructor({ subject, recipients }, content) {
