ba203a15fd04c9bbae87b50f69b9627a0ce2eee3
chaseb1985 <chasebarrier85@gmail.com>
2019-02-12 10:48:01 -0600

    work

---
+++ config/prod.js
@@ -0,0 +1,18 @@
+
+module.exports = {
+    google = {
+        clientID: process.env.GOOGLE_CLIENT_ID,
+        clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+
+    },
+    github = {
+        clientID: process.env.GITHUB_CLIENT_ID,
+        clientSecret: process.env.GITHUB_CLIENT_SECRET
+
+    },
+    mongoURL: process.env.MONGO_URI,
+    cookieKey: process.env.COOKIE_KEY,
+    stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+    stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+    sendGridKey: process.env.SEND_GRID_KEY,
+};
