13b5b97d6a6cc5d2f5286bb74d012c610d4e0846
chaseb1985 <chasebarrier85@gmail.com>
2019-02-14 10:52:38 -0600

    keroku build issue

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -2,19 +2,7 @@ const passport = require('passport');
 
 module.exports = app => {
     
-app.get('/auth/google', 
-    passport.authenticate('google', {
-        scope: ['profile', 'email']
-        //options obj, what kind of access user has(prof and email)
-        })
-);
-app.get(
-    '/auth/google/callback', 
-    passport.authenticate('google'),
-    (req, res) => {
-        res.redirect('/surveys');
-        }
-    );
+
 
 
 app.get('/api/logout', (req, res) => {
@@ -26,14 +14,7 @@ app.get('/api/logout', (req, res) => {
 app.get('/api/current_user', (req, res) => {
     res.send(req.user);
     }); 
-app.get('/auth/github',
-    passport.authenticate('github'),
-    function(req, res){});
-app.get('/auth/github/callback',
-    passport.authenticate('github', { failureRedirect: '/' }),
-    function(req, res) {
-    res.redirect('/surveys');
-    });
+
 };
 app.get('/google/redirect', (req, res) =>{
         res.send('you reached the callback uri');
